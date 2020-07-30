97f60a39a0c8ac01ee38a07b18434d36378b3c7d
Matt Clark <MATTHEWCLARK989@boisestate.edu>
2020-07-29 23:51:35 -0600

    Working High smoke model

--- Rcode/Residuals_Break_point.R
+++ Rcode/Residuals_Break_point.R
@@ -26,6 +26,7 @@ data_list <- list(
 
 options(mc.cores=8)
 
+rstan::rstan_options(autowrite=TRUE)
 
 
 #this converged
@@ -36,5 +37,36 @@ print( zz , probs=c( (1-0.89)/2 , 1-(1-0.89)/2 ) )
 
 
 save(zz,file="~/SmokeProject/ModelObjects/Residuals_bkpoint.rda")
+
+
+
+
+
+#subset by parks that had smoke >0.5sd
+high_smoke_parks<-dat%>%group_by(UnitCode)%>%
+  summarise(max_smoke=max(stdsmoke))%>%
+  filter(max_smoke>=0.5)
+
+high_smoke_parks<-dat%>%filter(UnitCode %in% high_smoke_parks$UnitCode)
+
+data_list_high_smoke <- list(
+  N = nrow(high_smoke_parks),
+  Nprk = length(unique(high_smoke_parks$UnitCode)),
+  vis = high_smoke_parks$VisDiff,
+  smoke = high_smoke_parks$stdsmoke,
+  pcode = as.numeric(as.factor(high_smoke_parks$UnitCode )))
+
+options(mc.cores=8)
+rstan::rstan_options(autowrite=TRUE)
+
+
+#this converged
+fit_HS<-stan( file="~/SmokeProject/StanCode/Residuals_Break_Point_HS.stan" , data=data_list_high_smoke,chains=8,iter=2000,control=list(adapt_delta=0.99,max_treedepth = 10) ,warmup = 1000 )
+print( fit_HS , probs=c( (1-0.89)/2 , 1-(1-0.89)/2 ) )
+
+
+save(fit_HS,file="~/SmokeProject/ModelObjects/Residuals_bkpoint_HS.rda")
+
+
 
 
